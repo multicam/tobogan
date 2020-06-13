a2efe9f4019128ea6a00e6c52f5f35422f3ea4b2
Multicam <jmgiorgi@gmail.com>
2020-06-13 18:17:00 +1000

    -- page 1/anim

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ import './_grid'
 import gsap from 'gsap'
 
 const tl = gsap.timeline();
-tl.to(".box", { duration: 1, x: 300 })
-  .to(".box", { duration: 1, backgroundColor: "#f38630" }, "+=0.5")
-  .to(".box", { duration: 1, x: 0, rotation: -360 }, "+=0.5")
+tl.to(".box", { duration: 1, x: '-100%', ease: "quad" })
+//   .to(".box", { duration: 1, backgroundColor: "#f38630" }, "+=0.5")
+//   .to(".box", { duration: 1, x: 0, rotation: -360 }, "+=0.5")

addac4cdfc31496953d50b72321b2c4027a875ef
multicam <jmgiorgi@gmail.com>
2020-06-15 08:28:21 +1000

    -- gitignore

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import './_grid'
 import gsap from 'gsap'
+// import gsap from './vendors'
 
 const tl = gsap.timeline();
-tl.to(".box", { duration: 1, x: '-100%', ease: "quad" })
+tl.to(".box", { duration: 1.25, x: '-100%', ease: "expo" })
+
 //   .to(".box", { duration: 1, backgroundColor: "#f38630" }, "+=0.5")
 //   .to(".box", { duration: 1, x: 0, rotation: -360 }, "+=0.5")

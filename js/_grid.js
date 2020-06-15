77f60b7a0ff889c5262382d4e2a1032f6d3b985c
Multicam <jmgiorgi@gmail.com>
2020-06-15 11:39:27 +1000

    -- page 1/grid flex

--- js/_grid.js
+++ js/_grid.js
@@ -11,6 +11,8 @@ function renderGrid () {
     const width = window.innerWidth, height = window.innerHeight, step = 16
 
     log('-- grid', width, height)
+    canvas.offsetTop = 0;
+    canvas.offsetLeft = 0;
     canvas.width = width
     canvas.height = height
 
--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ import gsap from 'gsap'
 // import gsap from './vendors'
 
 const tl = gsap.timeline();
-tl.to(".box", { duration: 1.25, x: '-100%', ease: "expo" })
+tl.to(".box", { duration: .75, width: '110%', ease: "expo" })
+tl.to(".box", { duration: 1.25, width: '0', ease: "expo" })
 
 //   .to(".box", { duration: 1, backgroundColor: "#f38630" }, "+=0.5")
 //   .to(".box", { duration: 1, x: 0, rotation: -360 }, "+=0.5")

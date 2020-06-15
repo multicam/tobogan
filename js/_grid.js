02ff9fdc5efcf60c4c1e01bca44bd32b718bb79a
multicam <jmgiorgi@gmail.com>
2020-06-15 10:50:17 +1000

    -- grid

--- js/_grid.js
+++ js/_grid.js
@@ -18,38 +18,38 @@ function renderGrid () {
     ctx.strokeStyle = 'rgba(200,0,0,.15)'
 
     //
-    // for( let x=0; x<width; x += step) {
-    //   ctx.beginPath()
-    //   ctx.moveTo(x,0)
-    //   ctx.lineTo(x,height)
-    //   ctx.stroke()
-    //   ctx.closePath()
-    // }
-
-    ctx.strokeStyle = 'rgba(0,0,200,.5)'
-
-    const
-      sidePad =  18,
-      gutter = 8,
-      colNum = 12,
-      colWidth = Math.floor((width - 2*sidePad)/12)
-
-    for( let n=0; n<=colNum; ++n )  {
-      let x = sidePad + n*colWidth
-
+    for( let x=0; x<width; x += step) {
       ctx.beginPath()
       ctx.moveTo(x,0)
       ctx.lineTo(x,height)
       ctx.stroke()
       ctx.closePath()
-
-      ctx.beginPath()
-      ctx.moveTo(x+gutter,0)
-      ctx.lineTo(x+gutter,height)
-      ctx.stroke()
-      ctx.closePath()
-
     }
+
+    // ctx.strokeStyle = 'rgba(0,0,200,.5)'
+    //
+    // const
+    //   sidePad =  18,
+    //   gutter = 8,
+    //   colNum = 12,
+    //   colWidth = Math.floor((width - 2*sidePad)/12)
+    //
+    // for( let n=0; n<=colNum; ++n )  {
+    //   let x = sidePad + n*colWidth
+    //
+    //   ctx.beginPath()
+    //   ctx.moveTo(x,0)
+    //   ctx.lineTo(x,height)
+    //   ctx.stroke()
+    //   ctx.closePath()
+    //
+    //   ctx.beginPath()
+    //   ctx.moveTo(x+gutter,0)
+    //   ctx.lineTo(x+gutter,height)
+    //   ctx.stroke()
+    //   ctx.closePath()
+    //
+    // }
 
 
     update = false;

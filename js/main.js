6928f24b66dd7e3f78cdd971a78d4dd175ee94a5
multicam <jmgiorgi@gmail.com>
2020-06-17 16:11:43 +1000

    -- anim text/4

--- js/main.js
+++ js/main.js
@@ -43,6 +43,7 @@ gsap.to(".slide_students .s3", {
 })
 
 const infinite = gsap.timeline({repeat:-1,paused: false})
+
 infinite.fromTo('.image .content span', {
     duration: 2.5,
     x:'100%',
@@ -53,28 +54,25 @@ infinite.fromTo('.image .content span', {
   }
 )
 
-gsap.to([".slide_courses .s1",".slide_courses .s3"], {
-  duration: 1,
-  startAt: {
-    translateY: '-400%'
-  },
-  translateX: '-100%',
-  translateY: '0',
 
-  scrollTrigger: {
-    trigger: ".slide_courses",
-    scrub: true
-  }
+const tl2 = gsap.timeline()
+
+tl2.from(".slide_courses .s1",{
+  rotateZ: '90deg',
+  translateY: '-400%',
+  transformOrigin: 'bottom right',
+  ease: "expo",
+
+
 })
 
-gsap
-  .to([".slide_courses .s2",".slide_courses .s4"], {
-    duration: 10,
+tl2.to([".slide_courses .s1",".slide_courses .s3"], {
+    duration: 1,
     startAt: {
-      translateX: '-100%',
       translateY: '-400%'
     },
-    translateX: '0%',
+
+    translateX: '-100%',
     translateY: '0',
 
     scrollTrigger: {
@@ -82,3 +80,18 @@ gsap
       scrub: true
     }
   })
+.to([".slide_courses .s2",".slide_courses .s4"], {
+  duration: 10,
+  startAt: {
+    translateX: '-100%',
+    translateY: '-400%'
+  },
+
+  translateX: '0%',
+  translateY: '0',
+
+  scrollTrigger: {
+    trigger: ".slide_courses",
+    scrub: true
+  }
+})

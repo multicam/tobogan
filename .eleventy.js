7ac7b422b8f8f5e2051546148874b15ea0ff9778
multicam <jmgiorgi@gmail.com>
2020-06-22 08:11:39 +1000

    -- sass chokidar/save

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,24 @@
-const chalk = require('chalk'), chokidar = require('chokidar'), promise = require('bluebird'), sass = require('node-sass'), path = require('path')
+const chalk = require('chalk'), chokidar = require('chokidar'), promise = require('bluebird'), sass = require('node-sass'), path = require('path'), fs = require('fs')
 
 const log = console.log, serialize = JSON.stringify, deserialize = JSON.parse, keysOf = Object.keys
 
 const sass_install = dir => {
 	log('sass install', dir)
 	chokidar.watch(dir).on('all', (event, path) => {
-		log(`processing ${path} on ${event}`)
+		log(`checking ${path} on ${event}`)
+		event === 'change' && sass_process(path).then( res => log('xxx', res))
 	});
 }
 
 const sass_process = file => {
-	return new promise((resolve, reject) => {
+
+	const pos = file.lastIndexOf("."), resFile = file.substr(0, pos < 0 ? file.length : pos) + ".css";
+	return pos !== -1 && new promise((resolve, reject) => {
 		sass.render({file: file}, (error, result) => {
-			error && reject(error);
-			resolve(result);
-			log(result)
+			error && log(error) && reject(error);
+			log('->',resFile, result)
+			result && result.css && fs.writeFileSync(resFile, result.css, 'utf8')
+			resolve(true);
 		});
 	});
 }
@@ -36,13 +40,7 @@ module.exports = function (eleventyConfig) {
 
 	sass_install('style/')
 
-	// promise.all([
-	// 	sass_process('style/index.scss'),
-	// 	sass_process('style/typography.scss')
-	// ]).then( res => {
-		eleventyConfig.addPassthroughCopy('style/*.css');
-	// })
-
+	eleventyConfig.addPassthroughCopy('style/*.css');
 	eleventyConfig.addPassthroughCopy('images');
   eleventyConfig.addPassthroughCopy('img');
 
--- _site/js/main.js
+++ _site/js/main.js
@@ -7096,7 +7096,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46733" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42675" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
